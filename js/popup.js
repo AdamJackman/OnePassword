788dd557123ea8da73953ec1de4c808554ebdc0a
AdamJackman <adam.jackman@mail.utoronto.ca>
2015-01-10 15:57:19 -0500

    Abstracting variables.
    Next step is to add database

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
+var length = 10;
 window.onload = function(){ 
 	// OnLoad setup
     document.getElementById("passBut").onclick = calcPass; 
@@ -7,18 +8,19 @@ function calcPass(){
 	// TODO: Cryptography
 //	var hash = CryptoJS.MD5("Message");
 //	alert(hash);
+	$("#passOut").val("");
+	var userInp = $("#passIn").val();
 
     //These are the three variables that are essential to the hashing process
     // might want to use something like this later for the salt - var salt = CryptoJS.lib.WordArray.random(128/8);
-    var salt = "hello";
-    var length = 5;
-    var userPass = "a";
+    var salt = "test";
+    //var length = 10;
+    var userPass = userInp;
 
     //Create the pass with the salt, the length, and the given password
 
     var key = CryptoJS.PBKDF2(userPass, salt, { keySize: 128/32 });
     var sizedPass = key.toString().substring(0,length);
 
-	//var inp = $("#passIn").val();
 	$("#passOut").val(sizedPass);
 }
