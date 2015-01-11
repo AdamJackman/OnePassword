a64172c604f5c1a6ef1e5870de5a68aa34e45acc
AdamJackman <adam.jackman@mail.utoronto.ca>
2015-01-10 19:04:34 -0500

    Set output length completed

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,72 @@
 var length = 10;
+
 window.onload = function(){ 
 	// OnLoad setup
     document.getElementById("passBut").onclick = calcPass; 
+    document.getElementById("lenset").onclick = setNewLength; 
 };
+
+function setNewLength(){
+	//Get the div to addTo
+	var addTo = document.getElementById("mainDiv");
+	//create the elements to enter the length into
+	var lenDiv = document.createElement('div');
+	lenDiv.setAttribute('id', "lenBox");
+	var inpLi = document.createElement('li');
+	inpLi.setAttribute('id', "inpLi");
+	var lenLab = document.createElement('label');
+	lenLab.setAttribute('id', "labBox");
+	lenLab.innerHTML = "Input the desired length";
+	var leninp = document.createElement('input');
+	leninp.setAttribute('id', "inpBox");
+	leninp.setAttribute('placeholder', "1 - 25");
+	var lenbut = document.createElement('button');
+	lenbut.setAttribute('id', 'inpBut');
+	lenbut.setAttribute('class', 'btn btn-default navbar-btn pull-right');
+	lenbut.setAttribute('type', 'button');
+	lenbut.innerHTML = "Set New Length";
+	lenbut.onclick = setLength;
+
+	//Create the input box in this
+
+	//Append the childen
+	$('body').append(lenDiv);
+	lenDiv.appendChild(inpLi);
+	inpLi.appendChild(lenLab);
+	inpLi.appendChild(leninp);
+	lenDiv.appendChild(lenbut);
+}
+
+function setLength(){
+	//Might want to do a little more error checking than this
+	var lencar =  document.getElementById("inpBox");
+	//var len = lencar.val();
+	var len = $("#inpBox").val();
+	alert(len);
+	if(len>25 || len<1){
+		alert("Invalid length entered");
+	}
+	else{
+		length = len;	
+	}
+	location.href= "";
+	/*
+	lenDiv.remove(lenbut);
+	inpLi.remove(leninp);
+	inpLi.remove(lenLab);	
+	lenDiv.remove(inpLi);
+	$('body').remove(lenDiv);
+	*/
+}
+
+function randomSymbol(){
+	//Can be used as a way to retrieve a single random symbol - for symbolic demands
+	possible = "!@#$%^&*()_+-=[]\\;',./{}|:<>?";
+	text = "";
+	text += possible.charAt(Math.floor(Math.random() * possible.length));
+	return text;
+}
+
 
 function calcPass(){
 	// TODO: Cryptography
